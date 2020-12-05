6c11e13d0605eedc4fe436d808dcb450975c0ce2
Samuel Mishra <samuelmishra@machine.local>
2020-12-05 01:12:44 -0500

    Better 4

--- Dec4/src/main/scala/Dec4/Records.scala
+++ Dec4/src/main/scala/Dec4/Records.scala
@@ -4,27 +4,22 @@ import scala.io.Source
 
 object Records {
 
-  def clean(data: List[String]): List[String] = {
-    data match {
-      case List(n) => List(n)
-      case n :: ns => {
-        (n, clean(ns)) match {
-          case ("", List(s))          => List("", s)
-          case (h: String, List(s))   => List(h ++ " " + s)
-          case ("", "" :: los)        => List("") ++ los
-          case ("", s :: los)         => List("") ++ List(s) ++ los
-          case (h: String, "" :: los) => List(h, "") ++ los
-          case (h: String, s :: los)  => List(h ++ " " ++ s) ++ los
-        }
-      }
+  def clean(data: Option[List[String]], accum: Option[List[String]]): (Option[List[String]], Option[List[String]]) = {
+    (data, accum) match {
+      case (_, None)                           => clean(data, Some(List("")))
+      case (Some(List()), Some(a))             => (None, Some(a))
+      case (Some(n), Some(a)) if n.head == ""  => clean(Some(n.tail), Some(a ++ List("")))
+      case (Some(n), Some(a)) if a.last == ""  => clean(Some(n.tail), Some(a ++ List(n.head)))
+      case (Some(n), Some(a))                  => clean(Some(n.tail), Some(a.init ++ List(a.last + " " + n.head)))
     }
   }
 
+
   def parse(data: List[String]): List[Map[String, String]] = {
-    data.filter(i => i != "").map { entry =>
+    data.filter(_ != "").map { entry =>
         entry.split(" ").map { kv =>
             Map(kv.split(":")(0) -> kv.split(":")(1))
-          }.reduce((m1, m2) => m1 ++ m2)
+          }.reduce(_++_)
     }
   }
 
@@ -33,15 +28,15 @@ object Records {
   }
 
   def strict_predicate(data: Map[String, String]): Boolean = {
-    val pattern1 = """^(\d{3})cm$""".r
-    val pattern2 = """^(\d{2})in$""".r
+    val cm = """^(\d{3})cm$""".r
+    val in = """^(\d{2})in$""".r
 
-    (((data("byr").toInt >= 1920) & (data("byr").toInt <= 2002))
+    (((data("byr").toInt >= 1920 & data("byr").toInt <= 2002))
     & ((data("iyr").toInt >= 2010) & (data("iyr").toInt <= 2020))
     & ((data("eyr").toInt >= 2020) & (data("eyr").toInt <= 2030))
     & (data("hgt") match {
-      case pattern1(x) => (x.toInt >= 150 & x.toInt <= 193)
-      case pattern2(x) => (x.toInt >= 59 & x.toInt <= 76)
+      case cm(x) => (x.toInt >= 150 & x.toInt <= 193)
+      case in(x) => (x.toInt >= 59 & x.toInt <= 76)
       case _           => false
     })
     & (data("hcl").matches("""^\#[a-f0-9]{6}$"""))
@@ -51,14 +46,13 @@ object Records {
 
   def predicate(data: List[Map[String, String]]): List[Int] = {
     data.map { m =>
-      if (loose_predicate(m)) {
-        if (strict_predicate(m)) 1  else 0
-      } else 0
+      if (loose_predicate(m)) { if (strict_predicate(m)) 1  else 0 }
+      else 0
     }
   }
 
   def handle(data: List[String]): Int = {
-    predicate(parse(clean(data))).sum
+    predicate(parse(clean(Some(data), None)._2.get)).sum
   }
 
   def main(args: Array[String]): Unit = {
