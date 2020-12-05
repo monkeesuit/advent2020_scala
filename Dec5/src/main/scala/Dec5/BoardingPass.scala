df6682cfe0ec7ecb05128932c2d4ffbeb9a5f300
Samuel Mishra <samuelmishra@machine.local>
2020-12-05 18:47:35 -0500

    fix typo

--- Dec5/src/main/scala/Dec5/BoardingPass.scala
+++ Dec5/src/main/scala/Dec5/BoardingPass.scala
@@ -4,27 +4,38 @@ import scala.io.Source
 
 object BoardingPass{
 
+
   def getSeatNum(seatCode: String): (Int, Int, Int) = {
+
     val binString = seatCode.toList.map{ ch =>
       ch match {
         case high if high.toString.matches("B|R") => "1"
         case low if low.toString.matches("F|L")   => "0"
       }
     }.mkString
-    val seatNum = Integer.parseInt(sb, 2)
+
+    val seatNum = Integer.parseInt(binString, 2)
     val rowNum = seatNum >> 3
     val colNum = seatNum & 7
+
     (seatNum, rowNum, colNum)
+
   }
 
+
   def handle(data: List[String]): Int = {
+
     val seatNums = data.map{getSeatNum(_)._1}
     val taken = (0 to (seatNums.min-1)) ++ seatNums ++ ((seatNums.max+1) to 1023)
     val missing = taken.reduce(_^_)
+
     missing
+
   }
 
+  
   def main(args: Array[String]): Unit = {
+
     val f = Source.fromFile(args(0))
     val data = f.getLines.toList
     f.close
